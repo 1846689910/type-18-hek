2be04c32b41fa0d069fe9453531bc7a78f82308b
1866689910 <zuhe75@gmail.com>
2018-09-08 16:05:08 -0700

    ...

--- webpack.config.js
+++ webpack.config.js
@@ -15,25 +15,30 @@ module.exports = {
                 }
             },{
                 test: /\.html$/,
-                use: [
-                    {
-                        loader: "html-loader",
-                        options: { minimize: true }
-                    }
-                ]
+                use: [{
+                    loader: "html-loader",
+                    options: { minimize: true }
+                }]
             },{
                 test: /\.css$/,
                 use: ["style-loader", "css-loader"]
             },{
                 test: /\.(png|jpg|gif)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            emitFile: true
-                        }
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        emitFile: true,
+                        name: './img/[name]-[hash].[ext]', // 这个相对路径是基于`${__dirname}/dist/...`
                     }
-                ]
+                }]
+            },{
+                test: /fonts\/.*\.(woff|woff2|eot|ttf|svg)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: `./fonts/[name]-[hash].[ext]`
+                    }
+                }]
             }
         ]
     },
