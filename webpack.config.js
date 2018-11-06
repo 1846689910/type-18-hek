aa48ed1c164f0a17133f0f9cce300bfc174d6dcd
1866689910 <zuhe75@gmail.com>
2018-11-05 22:26:34 -0800

    ...

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,13 @@ module.exports = {
         path: `${__dirname}/dist`,  // packed file directory
         filename: "bundle.[contenthash].js"  // name of packed file
     },
+    devtool: 'eval-source-map',
+    devServer: {
+        contentBase: "./dist",  // 默认webpack-dev-server会为根文件夹提供本地服务器，如果想为另外一个目录下的文件提供本地服务器，应该在这里设置其所在目录
+        historyApiFallback: true,  // 在开发单页应用时非常有用，它依赖于HTML5 history API，如果设置为true，所有的跳转将指向index.html
+        inline: true,  // 设置为true，当源文件改变时会自动刷新页面
+        port: 8080,  // 设置默认监听端口，如果省略，默认为”8080“
+    },
     optimization: {
         runtimeChunk: 'single',
         splitChunks: {
