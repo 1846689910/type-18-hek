a42fe9864a5f08f602e6603e3dbbc396fdbacbf5
1846689910 <zuhe75@gmail.com>
2019-02-21 23:14:42 -0800

    use only babelrc, skip babel-loader options

--- webpack.config.js
+++ webpack.config.js
@@ -67,30 +67,7 @@ module.exports = env => ({
         exclude: /node_modules/,
         use: [
           {
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/preset-env", "@babel/preset-typescript", "@babel/preset-react"],
-              cacheDirectory: true,
-              plugins: [
-                ["@babel/plugin-proposal-decorators", { legacy: true }],
-                "@babel/plugin-syntax-dynamic-import",
-                "transform-class-properties",
-                "css-modules-transform",
-                [
-                  "react-css-modules",
-                  {
-                    webpackHotModuleReloading: true,
-                    generateScopedName: `${
-                      (APP_SERVER && APP_SERVER.endsWith("dev")) || env.development
-                        ? "[name]__[local]___"
-                        : ""
-                    }[hash:base64:5]`
-                  }
-                ],
-                ["@babel/plugin-proposal-class-properties", { loose: true }],
-                "@babel/proposal-object-rest-spread"
-              ]
-            }
+            loader: "babel-loader"
           }
         ]
       },
