6df5d403429a036bfe2f061363e2d68add8c5f0e
1846689910 <zuhe75@gmail.com>
2019-03-15 11:34:34 -0700

    add @loadable/component

--- src/client/js/components/demo-dynamic-import.jsx
+++ src/client/js/components/demo-dynamic-import.jsx
@@ -1,51 +1,15 @@
 import React from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { setShowFakeComp } from "../settings/actions";
-import bootstrap from "bootstrap/dist/css/bootstrap.min.css"; // eslint-disable-line
-import Promise from "bluebird";
+import loadable from "@loadable/component";
 
-let Fake;
-const loadFakeComp = (dispatch, doRefresh = false) => {
-  if (!Fake || doRefresh) {
-    const ready = doRefresh
-      ? Promise.try(() => dispatch(setShowFakeComp(false))).delay(1000)
-      : Promise.try(() => {});
-    ready
-      .then(() => import("./demo-fake"))
-      .then(({ Fake: _Fake }) => {
-        Fake = _Fake;
-        dispatch(setShowFakeComp(true));
-      });
-  }
-};
+const NamedLazyComp = loadable(() =>
+  import("./demo-fake").then(module => ({ default: module.Fake }))
+);
+const DefaultLazyComp = loadable(() => import("./demo-fake"));
 
-const DynamicImportDemo = props => {
-  const { showFakeComp, dispatch } = props;
-  loadFakeComp(dispatch);
-  return (
-    <div>
-      <h6>Dynamic Import</h6>
-      {showFakeComp.value && Fake ? <Fake {...props} /> : <div>Fake Comp is loading ...</div>}
-      <button
-        styleName={"bootstrap.btn bootstrap.btn-primary"}
-        onClick={() => loadFakeComp(dispatch, true)}
-      >
-        Refresh Fake Comp
-      </button>
-    </div>
-  );
-};
-DynamicImportDemo.propTypes = {
-  showFakeComp: PropTypes.object,
-  dispatch: PropTypes.func
-};
-
-export default connect(
-  state => state,
-  dispatch => ({ dispatch })
-)(DynamicImportDemo);
-
-// if (module.hot) {
-//   module.hot.accept(); // TODO: HMR with Dynamic Import
-// }
+export default () => (
+  <div>
+    <h6>@loadable/component Dynamic Import</h6>
+    <NamedLazyComp />
+    <DefaultLazyComp />
+  </div>
+);
