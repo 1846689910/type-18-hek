7926f28e503dd4f07ed2fd31cf9f331ce25aecfe
1846689910 <38680030+1846689910@users.noreply.github.com>
2019-12-18 23:13:24 +0900

    [patch] update package and update Nav (#15)

--- src/client/js/components/Nav.jsx
+++ src/client/js/components/Nav.jsx
@@ -1,21 +1,21 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, createRef, useEffect } from "react";
+import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
 import {
   Container,
   Grid,
   AppBar,
-  Tabs,
-  Tab,
   makeStyles,
-  styled,
-  Typography
+  Typography,
+  Button,
+  ButtonGroup,
+  Menu,
+  MenuItem
 } from "@material-ui/core";
-import {
-  useHistory,
-  useLocation,
-  useParams,
-  useRouteMatch
-} from "react-router-dom";
-import { useSelector } from "react-redux";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import { useHistory } from "react-router-dom";
+
+import { theme } from "../../styles/theme";
 
 const useStyles = makeStyles({
   root: {
@@ -28,29 +28,66 @@ const useStyles = makeStyles({
     height: "100%"
   }
 });
-export const MyTab = styled(Tab)({
-  textTransform: "none",
-  color: "white",
-  fontWeight: "bold",
-  fontSize: "15px"
-});
+
+function PipelineDropdown(props) {
+  const { anchorEl, handleClose, dropdown } = props;
+  const refs = dropdown.jobIds.map(() => createRef());
+  const history = useHistory();
+  const handleClick = jobId => {
+    handleClose();
+    history.push(`/pipelines/123/jobs/${jobId}`);
+  };
+  useEffect(() => {
+    if (history.location.pathname.match(/\/jobs\/(.+)/)) {
+      const idx = dropdown.jobIds.findIndex(x => `${x}` === history.location.pathname.match(/\/jobs\/(.+)/)[1]);
+      if (idx >= 0) {
+        refs.forEach(x => x.current && (x.current.style.background = ""));
+        refs[idx].current &&
+          (refs[idx].current.style.background = theme.palette.secondary.main);
+      }
+    }
+  });
+  return (
+    <Menu
+      id="simple-menu"
+      anchorEl={anchorEl}
+      keepMounted
+      open={Boolean(anchorEl)}
+      onClose={handleClose}
+      style={{ marginTop: "45px" }}
+    >
+      {dropdown.jobIds.map((x, i) => (
+        <MenuItem key={i} onClick={() => handleClick(x, i)} ref={refs[i]}>
+          Job{x}
+        </MenuItem>
+      ))}
+    </Menu>
+  );
+}
+PipelineDropdown.propTypes = {
+  anchorEl: PropTypes.object,
+  handleClose: PropTypes.func,
+  dropdown: PropTypes.object
+};
 
 export default function Nav() {
   const classes = useStyles();
   const counter = useSelector(state => state.counter);
-  const tabs = ["/", "/demo1", `/demo2/${counter.value}`];
-  const location = useLocation();
+  const [jobAnchor, setJobAnchor] = useState(null);
+  const tabs = [
+    { pathname: "/", label: "Home" },
+    { pathname: "/demo1", label: "Demo1" },
+    {
+      pathname: "/pipelines/123",
+      label: "Pipelines",
+      dropdown: {
+        pathname: "/pipelines/123/jobs/:jobId",
+        jobIds: [123, 456, 789]
+      }
+    },
+    { pathname: `/demo2/${counter.value}`, label: "Demo2" }
+  ];
   const history = useHistory();
-  const [value, setValue] = useState(
-    tabs.find(x => x.startsWith(location.pathname))
-  );
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-    history.push(newValue);
-  };
-  const params = useParams();
-  const routeMatch = useRouteMatch();
-  console.log([history, location, params, routeMatch]);
   return (
     <Fragment>
       <Container className={classes.hc} maxWidth="md">
@@ -61,16 +98,62 @@ export default function Nav() {
         </Grid>
       </Container>
       <AppBar position="static" className={classes.root}>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          indicatorColor="secondary"
-          centered
-        >
-          <MyTab label="Home" value="/" />
-          <MyTab label="Demo1" value="/demo1" />
-          <MyTab label="Demo2" value={`/demo2/${counter.value}`} />
-        </Tabs>
+        <Container maxWidth="md">
+          <Grid
+            container
+            style={{ width: "100%", height: "50px" }}
+            alignItems="center"
+          >
+            <Grid container justify="center">
+              {tabs.map((x, i) => (
+                <Fragment key={i}>
+                  <ButtonGroup
+                    style={{
+                      margin: "0 20px"
+                    }}
+                  >
+                    <Button
+                      variant="contained"
+                      color={
+                        x.pathname === history.location.pathname ? "secondary" : "default"
+                      }
+                      onClick={() => history.push(x.pathname)}
+                      style={{
+                        fontWeight: "bold",
+                        textTransform: "none"
+                      }}
+                    >
+                      {x.label}
+                    </Button>
+                    {x.label === "Pipelines" ? (
+                      <Button
+                        variant="contained"
+                        color={
+                          x.pathname === "Pipelines" ? "secondary" : "default"
+                        }
+                        style={{ width: "20px" }}
+                        onClick={e => setJobAnchor(e.target)}
+                      >
+                        <ArrowDropDownIcon />
+                      </Button>
+                    ) : (
+                        ""
+                      )}
+                  </ButtonGroup>
+                  {x.label === "Pipelines" ? (
+                    <PipelineDropdown
+                      anchorEl={jobAnchor}
+                      handleClose={() => setJobAnchor(null)}
+                      dropdown={x.dropdown}
+                    />
+                  ) : (
+                      ""
+                    )}
+                </Fragment>
+              ))}
+            </Grid>
+          </Grid>
+        </Container>
       </AppBar>
     </Fragment>
   );
