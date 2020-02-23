979d39ee997b46eb9d3bc9339fa4ac03ac997d14
1846689910 <eric14706@gmail.com>
2020-02-22 18:30:30 -0800

    mod

--- browser-automation/index.js
+++ browser-automation/index.js
@@ -1,6 +1,5 @@
-const { Builder, By, Key, until } = require("selenium-webdriver");
+const { Builder, By, Key, until, Origin } = require("selenium-webdriver");
 const Promise = require("bluebird");
-require("selenium-webdriver/testing");
 require("chromedriver");
 
 (async () => {
@@ -11,28 +10,46 @@ require("chromedriver");
     return driver.wait(Promise.delay(ms).then(() => false), ms);
   }
 
-  try {
-    await driver.get("https://www.google.com/");
-    // await driver.findElement(By.name("q")).sendKeys("webdriver", Key.RETURN);
-    // await driver.wait(Promise.try(() => false), 1000);
-    await driverWaitTime(1000);
-    await driver.findElement(By.css("input[type=text]")).sendKeys("1846689910/type-18-hek", Key.RETURN);
-    await driver.wait(until.elementLocated(By.id("main")), 3000);
-    await driverWaitTime(1000);
-    await (await driver.findElement(By.css("body"))).click();
-    // console.log(driver.findElement(By.css("a[href='https://github.com/1846689910/type-18-hek']")));
-    await driver.actions().move({
-      duration: 500,
-      // origin: driver.findElement(By.css("a[href='https://github.com/1846689910/type-18-hek']")),
-      origin: driver.findElement(By.js(() => document.querySelector("a[href='https://github.com/1846689910/type-18-hek']")))
-    });
-    // (await driver.findElement(By.css("a[href='https://github.com/1846689910/type-18-hek']"))).click();
-    // await driver.wait(until.titleContains("1846689910/type-18-hek"), 3000);
+  /**
+   * @description ensure url or the current page <body> located
+   * @param {String} url load page url
+   * @param {Number} timeout the timeout ms for page loading
+   */
+  async function driverWaitUntilBodyLocated(url, timeout=3000, doFocus=true){
+    if (url) await driver.get(url);
+    const located = await driver.wait(until.elementLocated(By.css("body")), timeout);
+    return doFocus ? focusViewport() : located;
+  }
 
+  async function focusViewport(){
+    return actions
+      .move({
+        origin: Origin.VIEWPORT
+      })
+      .click()
+      .perform();
+  }
+
+  try {
+    await driverWaitUntilBodyLocated("https://www.google.com/");
+
+    await driver
+      .findElement(By.css("input[type=text]"))
+      .sendKeys("1846689910/type-18-hek", Key.RETURN);
+
+    await driverWaitUntilBodyLocated();
+
+    await actions
+      .move({
+        duration: 500,
+        origin: await driver.findElement(
+          By.css("a[href='https://github.com/1846689910/type-18-hek']")
+        )
+      })
+      .click().perform();
+    
     await driverWaitTime(3000);
 
-    // await driver.wait(Promise.delay(10000).then(() => false), 20000);
-    // await driver.wait(until.titleIs("webdriver - Google Search"), 1000);
   } finally {
     await driver.quit();
   }
