31adacdcf8db4476af38f205a6f868f28168679b
1846689910 <eric14706@gmail.com>
2020-03-07 00:35:08 -0800

    mod

--- src/client/js/pages/Home.jsx
+++ src/client/js/pages/Home.jsx
@@ -3,9 +3,9 @@ import PropTypes from "prop-types";
 import { Container, Grid } from "@material-ui/core";
 import Nav from "../components/Nav";
 import DemoWrapper from "../components/DemoWrapper";
-import ModuledStyleDemo from "../components/ModuledStyleDemo";
-import DynamicImportDemo from "../components/DynamicImportDemo";
-import RecomposeDemo from "../components/RecomposeDemo";
+import ModuledStyleDemo from "../components/Home/ModuledStyleDemo";
+import DynamicImportDemo from "../components/Home/DynamicImportDemo";
+import RecomposeDemo from "../components/Home/RecomposeDemo";
 
 
 export default function Home(props) {
