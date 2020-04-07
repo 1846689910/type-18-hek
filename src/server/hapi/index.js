66a0bcaa667af6b623a12988955fabcf8fed6e8c
1846689910 <eric14706@gmail.com>
2020-04-07 13:45:26 -0700

    configure hapi for subscriptions

--- src/server/hapi/index.js
+++ src/server/hapi/index.js
@@ -1,23 +1,35 @@
 import Path from "path";
 import Hapi from "hapi";
 import chalk from "chalk";
+import { apolloServerHapi } from "./graphql-middleware-hapi";
+import { publishServerTime } from "../utils/utils";
+
 const PORT = process.env.PORT || 3000;
 const staticRoot = Path.resolve("dist");
-import { apolloServerHapi } from "./graphql-middleware-hapi";
 
 const server = Hapi.server({
   port: PORT,
-  host: "localhost"
+  host: "localhost",
 });
+
 (async () => {
   await server.register([
     { plugin: require("./ssr-plugin"), options: { staticRoot } },
-    require("inert") // configure plugin inert to load static resources
+    require("inert"), // configure plugin inert to load static resources
   ]);
 
   await apolloServerHapi.applyMiddleware({ app: server });
 
+  await apolloServerHapi.installSubscriptionHandlers(server.listener);
+
   await server.start();
   
   console.log(chalk.bold.blue(`Hapi server running at: ${server.info.uri}`));
+  console.log(
+    chalk.yellow(`
+      - graphql at http://localhost:${PORT}${apolloServerHapi.graphqlPath}
+      - apollo graphql subscriptions at ws://localhost:${PORT}${apolloServerHapi.subscriptionsPath}
+  `),
+  );
+  publishServerTime();
 })();
