6177e8bd6fa07c67337ad122a8ed6be356fb1839
1846689910 <eric14706@gmail.com>
2020-07-30 14:31:35 -0700

    mod

--- src/server/express/index.js
+++ src/server/express/index.js
@@ -6,6 +6,7 @@ import chalk from "chalk";
 import Status from "http-status";
 
 const PORT = process.env.PORT || 3000;
+const touch = process.env.touch;
 const app = express();
 
 app.get("/alive", (_, res) => {
@@ -18,8 +19,12 @@ app.use(graphqlMiddleware2);
 
 app.get("*", ssrMiddleware);
 
-app.listen(PORT, () => {
+const running = app.listen(PORT, () => {
   console.log(
     chalk.bold.blue(`Express server is running at http://localhost:${PORT}`),
   );
+  if (touch && running.close) {
+    running.close();
+    process.exit(0);
+  }
 });
