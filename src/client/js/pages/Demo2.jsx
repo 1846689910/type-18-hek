8b0b7a143110c0743b5922c8e7520ae2b45661a9
1846689910 <38680030+1846689910@users.noreply.github.com>
2020-07-30 13:53:34 -0700

    update demo2 (#67)
    * mod
    
    * mod

--- src/client/js/pages/Demo2.jsx
+++ src/client/js/pages/Demo2.jsx
@@ -1,10 +1,22 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { Container, Grid } from "@material-ui/core";
-import { useLocation } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import Nav from "../components/Nav";
+import { get, isNaN } from "lodash";
+import { setCounterAction } from "../settings/actions";
 
 export default function Demo2() {
   const location = useLocation();
+  const dispatch = useDispatch();
+  const mainCounter = useSelector((state) => state.counter.value);
+  const params = useParams();
+  useEffect(() => {
+    const counter = parseInt(get(params, "id", 0));
+    if (mainCounter !== counter && !isNaN(counter)) {
+      dispatch(setCounterAction(counter));
+    }
+  }, []);  // eslint-disable-line
   return (
     <Fragment>
       <Nav />
