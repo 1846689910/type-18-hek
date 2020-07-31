c9ad3e8bd794f386455ef007b3343b736e414807
1846689910 <eric14706@gmail.com>
2020-07-31 16:59:36 -0700

    mod

--- src/client/js/pages/NoMatch.jsx
+++ src/client/js/pages/NoMatch.jsx
@@ -1,26 +1,33 @@
 import React, { useEffect, Fragment } from "react";
-import { Container, Grid, Typography } from "@material-ui/core";
+import { Container, Grid, Typography, styled } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
 import Nav from "../components/Nav";
 
 export default function NoMatch() {
   const history = useHistory();
   useEffect(() => {
-    setTimeout(() => history.push("/"), 500);
+    setTimeout(() => history.push("/"), 1000);
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
   return (
     <Fragment>
       <Nav />
       <Container maxWidth="md">
-        <Grid container justify="center" direction="column">
-          <Grid container justify="center">
-            <Typography component="h1">Not Found</Typography>
+        <Wrapper container justify="center" alignItems="center">
+          <Grid container direction="column">
+            <Grid container justify="center">
+              <Typography variant="h4">404 Not Found</Typography>
+            </Grid>
+            <Grid container justify="center">
+              <Typography variant="h5">
+                routing back to home page
+              </Typography>
+            </Grid>
           </Grid>
-          <Grid container justify="center">
-            <Typography component="h3">Will route back to home page</Typography>
-          </Grid>
-        </Grid>
+        </Wrapper>
       </Container>
     </Fragment>
   );
 }
+const Wrapper = styled(Grid)({
+  height: "60vh"
+});
